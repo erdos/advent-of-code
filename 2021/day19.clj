f7aeca8c495300d6fc4dba28ea01eabd213c9a69
erdos <erdosj@gmail.com>
2021-12-19 21:37:35 +0100

    feat: performance improvements for day 19

--- 2021/day19.clj
+++ 2021/day19.clj
@@ -37,22 +37,6 @@
 
 (defn translate [[a b c] [x y z]] [(- x a) (- y b) (- z c)])
 
-(defn try-connect [k1 k2]
-  (let [points1 (get scanners k1)
-        points2 (get scanners k2)]
-    (first
-     (for [pivot1 points1
-           :let [pts1 (for [p points1] (translate pivot1 p))
-                 pts1s (set pts1)]
-           rotator camrotators
-           pivot2 points2
-           :let [pts2 (for [p points2] (rotator (translate pivot2 p)))
-                 common-pts (filter pts1s pts2)]
-           :when (<= 12 (count common-pts))
-           :let [pts1->pt (zipmap pts1 (for [[x y z] points1] [k1 x y z]))
-                 pts2->pt (zipmap pts2 (for [[x y z] points2] [k2 x y z]))]]
-       (mapv (juxt pts1->pt pts2->pt) common-pts)))))
-
 ;; returns a function that maps points from k2 into k1
 (defn try-connect-fn [k1 k2]
   (let [points1 (get scanners k1)
@@ -70,31 +54,26 @@
 (def try-connect-fn (memoize try-connect-fn))
 
 (let [dist-signature (memoize (fn [k] (set (for [a (scanners k) b (scanners k)] (dist a b)))))]
-  (def potential? (memoize (fn [k1 k2]
-                             (let [k1d (dist-signature k1) k2d (dist-signature k2)]
-                               (< 12 (count (filter k1d k2d))))))))
-
-
-(def all-scanner-pairs (set (for [[a] scanners [b] scanners :when (not= a b)]
-                              (vec (sort [a b])))))
+  (defn potential?  [k1 k2]
+    (< 30 (count (filter (dist-signature k1) (dist-signature k2)))))
+  (def potential? (memoize potential?)))
 
 (def potential-children
-  (reduce (fn [m [a b]] (if (potential? a b)
-                          (-> m (update b conj a) (update a conj b)) m))
-          {} all-scanner-pairs))
+  (->> (for [[a] scanners [b] scanners :when (not= a b)] [a b])
+       (reduce (fn [m [a b]] (if (potential? a b) (-> m (update b conj a) (update a conj b)) m)) {})))
 
 (def dir-tree
-  (time
-   (loop [tree-edges ()
-          tree-nodes #{} ;; nodes already added to tree
-          node-queue #{"--- scanner 0 ---"}]
-     (if-let [n (first node-queue)]
-       (let [targets (filter (fn [e] (try-connect-fn n e)) ;; TODO: perhaps flip it
-                             (remove tree-nodes (potential-children n)))]
-         (recur (into tree-edges (for [e targets] [e n]))
-                (into tree-nodes (cons n targets))
-                (into (disj node-queue n) targets)))
-       (reduce (fn [m [k v]] (update m k assoc v (try-connect-fn v k))) {} tree-edges)))))
+  (loop [tree-edges ()
+         tree-nodes #{} ;; nodes already added to tree
+         node-queue #{"--- scanner 0 ---"}]
+    (if-let [n (first node-queue)]
+      (let [targets (->> (potential-children n)
+                         (remove tree-nodes)
+                         (filter (partial try-connect-fn n)))]
+        (recur (into tree-edges (for [e targets] [e n]))
+               (into tree-nodes (cons n targets))
+               (into (disj node-queue n) targets)))
+      (reduce (fn [m [k v]] (update m k assoc v (try-connect-fn v k))) {} tree-edges))))
 
 ;; returns a function that tells how to map to zero.
 (defn floodmap [real-children from]
@@ -112,16 +91,14 @@
        (f p))
      (set)
      (count)
-     (time)
      (println "First answer:"))
 
-(def scanner->fun (into {} (for [s (keys scanners)]
-                             [s (floodmap dir-tree s)])))
+(def scanner->fun
+  (into {} (for [s (keys scanners)] [s (floodmap dir-tree s)])))
 
 (let [scanner-positions (for [[s f] scanner->fun] (f [0 0 0]))]
   (->> (for [p1 scanner-positions
              p2 scanner-positions]
          (manhattan p1 p2))
        (apply max)
-       (time)
        (println "Second answer:")))
