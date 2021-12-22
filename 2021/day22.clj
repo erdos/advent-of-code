05cb46890a5e4e4f89e46f34c1e9f439e8dd0760
erdos <erdosj@gmail.com>
2021-12-22 22:22:28 +0100

    2021 day 22 part 2 solved

--- 2021/day22.clj
+++ 2021/day22.clj
@@ -1,12 +1,15 @@
 #!/usr/bin/env bb
 
+(set! *warn-on-reflection* true)
+
 (require '[clojure.string :as s])
 
-(defn ->long [x] (Long/valueOf x))
+(defn ->long [x] (Long/valueOf ^String x))
+(defn third [x] (nth x 2))
 
 (def items (for [[c & xs]
-                 (partition 7 (re-seq #"on|off|-\d+|\d+" (slurp "input22test3.txt")))]
-             [(keyword c) (partition 2 (map ->long xs))]))
+                 (partition 7 (re-seq #"on|off|-\d+|\d+" (slurp "input22.txt")))]
+             [(keyword c) (mapv vec (partition 2 (map ->long xs)))]))
 
 (defn points [[[a b] [c d] [e f]]]
   (for [i (range (max a -50) (inc (min 50 b)))
@@ -27,88 +30,107 @@
 ;; SECOND PART
 
 (defn realpoints [& triples]
-  (filter (fn [[a b cmd]] (<= a b)) triples))
+  (filter (fn [[cmd a b]] (<= a b)) triples))
 
-(defn overlap-segment [[a b] [c d]]
-  #_(println :overlap-seg a b c d)
+(defn overlap-segment [[a b :as a+b] [c d :as c+d]]
   {:pre [(<= a b) (<= c d)]
-   :post (every? (for [[x y _] %] (<= x y)))}
+   :post [(every? true? (for [[_ x y] %] (<= x y)))]}
   (cond
 
     ;; no intersection
-    (< b c) (list [a b :first] #_[b c :none] [c d :second])
-    (< d a) (list [c d :second] #_[d a :none] [a b :first])
+    (or (< b c) (< d a)) (list (cons :first a+b) (cons :second c+d))
 
-    ;; little intersection
-    (<= a c b d) (realpoints [a (dec c) :first] [c b :both] [(inc b) d :second])
-    (<= c a d b) (realpoints [c (dec a) :second] [a d :both] [(inc d) b :first])
+    ;; partial overlap
+    (<= a c b d) (realpoints [:first a (dec c)] [:both c b] [:second (inc b) d])
+    (<= c a d b) (realpoints [:second c (dec a)] [:both a d] [:first (inc d) b])
 
     ;; one contains another
-    (<= a c d b) (realpoints [a (dec c) :first] [c d :both] [(inc d) b :first])
-    (<= c a b d) (realpoints [c (dec a) :second] [a b :both] [(inc b) d :second])
+    (<= a c d b) (realpoints [:first a (dec c)] (cons :both c+d) [:first (inc d) b])
+    (<= c a b d) (realpoints [:second c (dec a)] (cons :both a+b) [:second (inc b) d])
 
     :else (assert false (str "Unexpected coords " (pr-str [[a b] [c d]])))))
 
 (defn same-code? [a b c] (->> [a b c] (remove #{:both}) ((fn [x] (or (empty? x) (apply = x))))))
 (def same-code (memoize same-code?))
 
-(defn union-cubes [xs ys]
-  (let [[a b c] (map overlap-segment xs ys)]
-    (for [[x1 x2 xcode] a
-          [y1 y2 ycode] b
-          [z1 z2 zcode] c
-          :when (same-code? xcode ycode zcode)]
-      [[x1 x2] [y1 y2] [z1 z2]])))
-
 (defn subtract-cubes [xs ys]
   (let [[a b c] (map overlap-segment xs ys)]
-    ;(println :subtract-cubes a b c)
-    (for [[x1 x2 xcode] a
-          [y1 y2 ycode] b
-          [z1 z2 zcode] c
-          :when (and ;(contains? #{:first} [xcode ycode zcode])
-                 (not (every? #{:both} [xcode ycode zcode]))
-                 (not-any? #{:second} [xcode ycode zcode]))]
+    (for [[xcode x1 x2] a
+          [ycode y1 y2] b
+          [zcode z1 z2] c
+          :when (and (or (= :first xcode) (= :first ycode) (= :first zcode))
+                     (and (not= :second xcode) (not= :second ycode) (not= :second zcode)))]
       [[x1 x2] [y1 y2] [z1 z2]])))
 
-;; subtract from itself
-(assert (empty? (subtract-cubes [[4 5] [4 5] [4 5]] [[4 5] [4 5] [4 5]])))
 
-;; total coverage
-(assert (empty? (subtract-cubes [[4 5] [4 5] [4 5]] [[1 10] [1 10] [1 10]])))
+(assert (empty? (subtract-cubes [[4 5] [4 5] [4 5]] [[4 5] [4 5] [4 5]]))) ;; subtract from itself
+(assert (empty? (subtract-cubes [[4 5] [4 5] [4 5]] [[1 10] [1 10] [1 10]]))) ;; total coverage
+(assert (= 7 (count (subtract-cubes [[4 5] [4 5] [4 5]] [[4 4] [4 4] [4 4]])))) ;; just removes one pt from the corner
 
-;; just removes one pt
-;; NEM JO!
-(assert (= 7 (count (subtract-cubes [[4 5] [4 5] [4 5]]
-                                    [[4 4] [4 4] [4 4]])))
-        (pr-str (subtract-cubes [[4 5] [4 5] [4 5]]
-                                [[4 4] [4 4] [4 4]]))
-        )
+;; hole in the middle
+(assert (= 26 (count (subtract-cubes [[1 10] [1 10] [1 10]] [[4 5] [4 5] [4 5]]))))
 
-;(assert false)
-;; hollow in the moddle
-#_(assert (= 8 (count (subtract-cubes [[1 10] [1 10] [1 10]]
-                                    [[4 5] [4 5] [4 5]])))
-        (pr-str (subtract-cubes [[1 10] [1 10] [1 10]]
-                                [[4 5] [4 5] [4 5]]))
-        )
+(defn cube-volume [sides] (reduce * 1N (map (fn [[a b]] (inc (- b a))) sides)))
 
+;; if one cube ends with z same as next then two can be merged
+(defn- merge-neighbors [sorted-cubes index]
+  ((fn f [last-cube cubes]
+     (assert (vector? last-cube) (str "Not vec " (pr-str last-cube)))
+     (if-let [[c & cs] (seq cubes)]
+       (if (= (inc (get-in last-cube [index 1])) (get-in c [index 0])) ;; same z
+         (let [merged-cube (assoc-in last-cube [index 1] (get-in c [index 1]))]
+           (recur merged-cube cs))
+         (cons last-cube (lazy-seq (f c cs))))
+       [last-cube]))
+   (first sorted-cubes) (next sorted-cubes)))
 
-(defn cube-volume [sides] (reduce * (map (fn [[a b]] (inc (- b a))) sides)))
+;; return a seq of cubes where some are merged
+(defn simplify-shards-z [cubes]
+  (for [g (vals (group-by (juxt first second) cubes))
+        :let [sorted-cubes (sort-by (fn [[_ _ [zmin zmax]]] zmax) g)]
+        e (merge-neighbors sorted-cubes 2)]
+    e))
+
+(defn simplify-shards-y [cubes]
+  (for [g (vals (group-by (juxt first third) cubes))
+        :let [sorted-cubes (sort-by (fn [[_ [ymin ymax] _]] ymax) g)]
+        e (merge-neighbors sorted-cubes 1)]
+    e))
+
+(defn simplify-shards-x [cubes]
+  (for [g (vals (group-by (juxt second third) cubes))
+        :let [sorted-cubes (sort-by (fn [[[xmin xmax] _ _]] xmax) g)]
+        e (merge-neighbors sorted-cubes 0)]
+    e))
+
+(defn simplify-shards [cubes]
+  (-> cubes simplify-shards-z simplify-shards-y simplify-shards-x))
+
+(defn fixpt [f x] (let [fx (f x)] (if (= fx x) fx (recur f fx))))
+
+(def simplify-shards-fix (partial fixpt simplify-shards))
+(def subtract-cubes (comp simplify-shards-fix subtract-cubes))
+
+(defn pmapcat [f xs] (apply concat (pmap f xs)))
+
+(defn add-to-cubes [cubes new-cube]
+  (let [new-shards
+        ;; remove existing pts from new cube
+        (reduce (fn [new-shards old-shard]
+                  (doall (pmapcat (fn [nss] (subtract-cubes nss old-shard)) new-shards)))
+                [new-cube] cubes)]
+    (into cubes (doall new-shards))))
 
 (->> items
-     (reduce (fn [s [cmd region]]
-               ; (assert (= (count s) (count (set s))))
-               (println :state-size (count s))
-               #_(println " " :state-volume (->> s (mapcat points) count))
-               #_(println :state s)
-               (case cmd
-                 :on (if (empty? s)
-                       (list region)
-                       (reduce (fn [regions shard]
-                                 (set (mapcat (partial union-cubes shard) regions)))
-                               #{region} s))
-                 :off (set (mapcat (fn [oldcube] (subtract-cubes oldcube region)) s))))
+     (reduce (fn [s [cmd new-cube]]
+               (->>
+                (case cmd
+                  :on  (add-to-cubes s new-cube)
+                  :off (set (pmapcat (fn [oldcube] (subtract-cubes oldcube new-cube)) s)))
+                (fixpt simplify-shards)))
              #{})
-     ;(mapcat points) (set) (count) (println :testing-second-answer)
-     (map cube-volume) (reduce +) (println "Second answer"))
+     (map cube-volume)
+     (reduce +)
+     (println "Second answer"))
+
+(shutdown-agents)
