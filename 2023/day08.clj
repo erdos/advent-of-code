d89fe0e63aee415c77e79c5be10afe477b7b60b0
Janos Erdos <janos.erdos@wise.com>
2023-12-08 13:23:03 +0100

    day08: rename vars to make more readable

--- 2023/day08.clj
+++ 2023/day08.clj
@@ -2,29 +2,30 @@
 
 (load-file "common.clj")
 
-(def command (first lines))
+(def instructions (first lines))
 
 ;; map of {location [left-loc right-loc]}
-(def parsed 
-  (into {} (for [line (nnext lines)]
-             (let [[a _ [left right]] (read-string (str  "(" line ")"))]
-               [a [left right]]))))
+(def tree 
+  (into {} (for [line (nnext lines)
+                 :let [[_ node left right] (re-matches #"(...) = \((...), (...)\)" line)]]
+             [node [left right]])))
 
-(defn solve1 [end? start-loc]
-  (->> (cycle command)
-       (reductions (fn [loc lr] 
+(defn solve1 [end? root]
+  (->> (cycle instructions)
+       (reductions (fn [loc i] 
                      (if (end? loc)
                        (reduced loc)
-                       (get-in parsed [loc (case lr \L 0 \R 1)])))
-                   start-loc)
+                       (get-in tree [loc (case i \L 0 \R 1)])))
+                   root)
        (count) (dec) (dec)))
 
-(println "First:" (solve1  #{'ZZZ} 'AAA))
+(println "First:" (solve1  #{"ZZZ"} "AAA"))
 
-(def start-nodes (filter (comp #{\A} last name) (keys parsed)))
+(defn start-node? [n] (str/ends-with? (name n) "A"))
 (defn end-node? [n] (str/ends-with? (name n) "Z"))
 
-(->> start-nodes
+(->> (keys tree)
+     (filter start-node?)
      (map (partial solve1 end-node?))
      (reduce lcm)
      (println "Second:"))
