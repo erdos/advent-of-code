453e2bc809422de3507f107a5bc42561e4382d9f
Janos Erdos <janos.erdos@wise.com>
2023-12-08 08:22:53 +0200

    rm unused code

--- 2023/day08.clj
+++ 2023/day08.clj
@@ -13,20 +13,16 @@
 (defn solve1 [end? start-loc]
   (->> (cycle command)
        (reductions (fn [loc lr] 
-                     (if (end? loc) (reduced loc)
-                       (case lr \L (get-in parsed [loc 0])
-                                \R (get-in parsed [loc 1]))))
-                   start-loc) 
+                     (if (end? loc)
+                       (reduced loc)
+                       (get-in parsed [loc (case lr \L 0 \R 1)])))
+                   start-loc)
        (count) (dec) (dec)))
 
 (println "First:" (solve1  #{'ZZZ} 'AAA))
 
 (def start-nodes (filter (comp #{\A} last name) (keys parsed)))
 (defn end-node? [n] (str/ends-with? (name n) "Z"))
-
-(defn step [lr loc]
-  (case lr \L (get-in parsed [loc 0])
-           \R (get-in parsed [loc 1])))
 
 (->> start-nodes
      (map (partial solve1 end-node?))
