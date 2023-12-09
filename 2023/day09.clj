754222e6b5c2a17f0aea379c84d557e7802576aa
Janos Erdos <janos.erdos@wise.com>
2023-12-09 06:27:14 +0100

    y23d09 - add alternative solutions

--- 2023/day09.clj
+++ 2023/day09.clj
@@ -13,12 +13,26 @@
     (first numbers)
     (+ (last numbers) (solve1 (diffs numbers)))))
 
+#_ ;; alternatively, with reduce:
+(defn solve1 [numbers]
+  (reduce (fn [sum numbers] (+ sum (last numbers))) 
+          0 (take-while (partial apply not= 0) (iterate diffs numbers))))
+
+#_ ;; alternatively, with loop-recur:
+(defn solve1 [numbers]
+  (loop [numbers numbers
+         acc    0]
+    (if (apply = numbers)
+      (+ (first numbers) acc)
+      (recur (diffs numbers) (+ acc (last numbers))))))
+
 (->> lines
      (map parse-line)
      (map solve1)
      (reduce +)
      (println "First"))
 
+;; second part is the same really, only we subtract the diff from the first nr
 (defn solve2 [numbers]
   (if (apply = numbers)
     (first numbers)
