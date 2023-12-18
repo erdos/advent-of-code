20800a5b290a7cd2735a0d50d77e1d4642d1a2e1
Janos Erdos <janos.erdos@wise.com>
2023-12-18 18:18:59 +0100

    solve 23 day 18

---
+++ 2023/day18.clj
@@ -0,0 +1,26 @@
+(ns day18)
+
+(load-file "common.clj")
+
+(defn parse [line]
+  (let [[_ [a] b c] (re-matches #"(.) (\d+) \(#(.{6})\)" line)]
+    [a (parse-long b) c]))
+
+(def plan (map parse lines))
+
+(defn solve1 [plan]
+  (second (reduce (fn [[h area] [dir len]]
+                    (case dir
+                      \U [(- h len) area]
+                      \D [(+ h len) (+ area len)]
+                      \L [h (+ area len (* h len))]
+                      \R [h (- area (* h len))]))
+                  [0 1] plan)))
+
+(->> plan solve1 (println "First"))
+
+(defn map-plan [[_ _ dir]]
+  [((zipmap "0123" "RDLU") (last dir))
+   (read-string (apply str "0x" (butlast dir)))])
+
+(->> plan (map map-plan) solve1 (println "Second"))
