ffe089b9d76c1824c5b8cae57dc6511757bff2ce
Janos Erdos <janos.erdos@wise.com>
2024-12-25 06:38:10 +0100

    solve 2024 day25

---
+++ 2024/day25.clj
@@ -0,0 +1,21 @@
+(ns day25)
+
+(def grids
+  (for [input (.split (slurp *in*) "\n\n")]
+    (mapv vec (.split input "\n"))))
+
+(defn pin-heights [grid]
+  (->> grid
+       (apply map vector) ;; transpose
+       (map #(filter #{\#} %))
+       (map count)
+       (mapv dec)))
+
+(def locks+keys (group-by ffirst grids))
+
+(->> (for [a (map pin-heights (locks+keys \#))
+           b (map pin-heights (locks+keys \.))
+           :when (not-any? (partial < 5) (map + a b))]
+       1)
+     (reduce +)
+     (println 'First))
